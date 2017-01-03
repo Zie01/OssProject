e3d538a3c4fa788545026e5dfc54745262491d61
CupcakeZeera <yragrande01@gmail.com>
2017-01-02 18:56:45 -0800

    Bug fixed:

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -4,12 +4,11 @@
 	<title>@yield ('title')</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
-    <body>
-		@includes('includes.header')
+    <body> 
+		@include('includes.header')
         <div class="container">
 			@yield('content')
-				
-		  </div>
+		</div>
     </body>  
 </html>
  
