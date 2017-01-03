3f5e91339758789e606390c1b3280085258f1204
CupcakeZeera <yragrande01@gmail.com>
2017-01-02 17:30:47 -0800

    added include

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -2,14 +2,13 @@
 <html>
     <head>
 	<title>@yield ('title')</title>
-        <style>
-           
-        </style>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
     <body>
+		@includes('includes.header')
         <div class="container">
 			@yield('content')
 				
 		  </div>
-    </body>
+    </body>  
 </html>
