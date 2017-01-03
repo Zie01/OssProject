6b5020428171e6f4d8c3e231c0f0b41d8cd650d0
CupcakeZeera <yragrande01@gmail.com>
2017-01-02 17:56:08 -0800

    created Sign UP/IN

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,40 @@
 @endsection
 
 @section('content')
-	This is the content!
+	<div class = "row">
+		<div class = "col-6">
+		<h3>Sign Up</h3>
+			<form action= "#" method="post">
+				<div class= "form-group">
+					<label for="email">Your E-Mail</label>
+					<input class= "form-control" type="text" name= "email" id="email">
+				</div>
+				<div class= "form-group">
+					<label for="email">First Name</label>
+					<input class= "form-control" type="text" name= "first_name" id="first_name">
+				</div>
+				<div class= "form-group">
+					<label for="email">Password</label>
+					<input class= "form-control" type="text" name= "Password" id="Password">
+				</div>
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</form>
+		</div>		
+		<div class = "col-6">
+		<h3>Sign In</h3>
+			<form action= "#" method="post">
+				<div class= "form-group">
+					<label for="email">Your E-Mail</label>
+					<input class= "form-control" type="text" name= "email" id="email">
+				</div>
+				
+				<div class= "form-group">
+					<label for="email">Password</label>
+					<input class= "form-control" type="text" name= "Password" id="Password">
+				</div>
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</form>
+		</div>	
+	</div>			
 @endsection
 
