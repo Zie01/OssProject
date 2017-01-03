217ad23518ef015eb536e76518259527a7fe1f85
CupcakeZeera <yragrande01@gmail.com>
2017-01-02 17:08:16 -0800

    master blade

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<title>@yield ('title')</title>
+        <style>
+           
+        </style>
+    </head>
+    <body>
+        <div class="container">
+			@yield('content')
+				
+		  </div>
+    </body>
+</html>
